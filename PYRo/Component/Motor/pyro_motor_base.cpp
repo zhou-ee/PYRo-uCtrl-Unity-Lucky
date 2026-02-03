cac3d5e7bdeb5056f2ad2d39b3b99ea283be8f21
lucky <zhou_20006ee@163.com>
2026-02-03 20:57:35 +0800

    ready merge gimbal

--- PYRo/Component/Motor/pyro_motor_base.cpp
+++ PYRo/Component/Motor/pyro_motor_base.cpp
@@ -38,10 +38,4 @@ bool motor_base_t::is_enable(void)
     return _enable;
 }
 
-// Provide the virtual destructor definition so the vtable is emitted
-motor_base_t::~motor_base_t()
-{
-    // base class cleanup (none currently)
-}
-
 } // namespace pyro
